605aba2a2a641ac1447f4c87f6fae6af398c6a34
Mike Dolan <mikedolan03@gmail.com>
2018-03-29 07:00:44 -0400

    created a blogpost model for db

---
+++ models.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const blogPostSchema = mongoose.Schema ({
+	title: {type: String, required: true},
+	content: {type: String, required: true},
+	author: {type: String, required: true},
+	created: {type: Date, required: true}
+
+});
+
+blogPostSchema.virtual('authorObj').get(function() {
+
+	let author = this.author;
+	let authorArray = author.split(" ");
+
+	let authorObject = { 
+		firstname: authorArray[0],
+		lastname: authorArray[1]
+	};
+
+  return authorObject;
+
+});
+
+
+blogPostSchema.methods.serialize = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorObj,
+    created: this.created
+  };
+}
+
+// note that all instance methods and virtual properties on our
+// schema must be defined *before* we make the call to `.model`.
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+module.exports = {BlogPost};
